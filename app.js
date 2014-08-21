43406c581f89c664d97e724080b09bb3992b762a
Dan Pawluk <dpawluk@zendesk.com>
2014-08-21 16:39:26 -0700

    working notify

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
           return {
             url: '/api/v2/apps/notify',
             type: 'POST',
+            contentType: 'application/json',
             data: JSON.stringify({
               "app_id": this.id(),
               "event": "popMessage",
@@ -18,6 +19,7 @@
     events: {
       'app.activated':'doSomething',
       'ticket.save': 'checkUrgent',
+      'notification.popMessage':'doPopMessage'
 
     },
 
@@ -28,13 +30,13 @@
     checkUrgent: function() {
       var ticket = this.ticket();
       if (ticket.priority() == "urgent") {
-        var data = "the ticket #" + ticket.id() + "has been marked as urgent priority and needs attention";
+        var data = "the ticket #" + ticket.id() + " has been marked as urgent priority and needs attention";
         this.ajax("notifyAll", data);
         return true;
       }
     },
 
-    popMessage: function(body) {
+    doPopMessage: function(body) {
       this.$('.message').text(body);
     }
 
