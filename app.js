2f6877e377b710343066d2f62a9b060e859a5e66
Dan Pawluk <dpawluk@zendesk.com>
2014-08-21 16:10:07 -0700

    first try

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
 (function() {
 
   return {
+    requests: {
+        notifyAll: function(data) {
+          return {
+            url: '/api/v2/apps/notify',
+            type: 'POST',
+            data: JSON.stringify({
+              "app_id": this.id(),
+              "event": "popMessage",
+              "body": data
+            })
+          };
+        }
+    },
+
     events: {
-      'app.activated':'doSomething'
+      'app.activated':'doSomething',
+      'ticket.save': 'checkUrgent',
+
     },
 
     doSomething: function() {
+      this.switchTo('main');
+    },
+
+    checkUrgent: function() {
+      var ticket = this.ticket();
+      if (ticket.priority() == "urgent") {
+        var data = "the ticket #" + ticket.id() + "has been marked as urgent priority and needs attention";
+        this.ajax("notifyAll", data);
+        return true;
+      }
+    },
+
+    popMessage: function(body) {
+      this.$('.message').text(body);
     }
+
   };
 
 }());
